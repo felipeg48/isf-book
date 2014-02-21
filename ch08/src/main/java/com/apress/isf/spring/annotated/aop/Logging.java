1ec3f0e1eac8885ec397bf904afc06a28012d54a
felipeg48 <fgutierrezcru@vmware.com>
2014-02-21 09:43:07 -0800

    initial commit for CH08

---
+++ ch08/src/main/java/com/apress/isf/spring/annotated/aop/Logging.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.apress.isf.spring.annotated.aop;
+
+/**
+ * @author Felipe Gutierrez
+ *
+ */
+public class Logging {
+
+}
---
+++ ch08/src/main/java/com/apress/isf/spring/aop/CachingModule.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.apress.isf.spring.aop;
+
+/**
+ * @author Felipe Gutierrez
+ *
+ */
+public class CachingModule {
+
+}
