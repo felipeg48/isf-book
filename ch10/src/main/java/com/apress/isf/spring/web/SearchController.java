2994374b2009b26aeb390d2e7aa987417f485ca3
felipeg48 <fgutierrezcru@vmware.com>
2014-03-04 21:29:33 -0700

    updated

--- ch10/src/main/java/com/apress/isf/spring/web/SearchController.java
+++ ch10/src/main/java/com/apress/isf/spring/web/SearchController.java
@@ -3,15 +3,12 @@
  */
 package com.apress.isf.spring.web;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.apress.isf.java.model.Document;
 import com.apress.isf.spring.data.DocumentDAO;
 
 /**
@@ -26,10 +23,8 @@ public class SearchController {
 	DocumentDAO documentDAO;
 
 	@RequestMapping(value="/all",method=RequestMethod.GET)
-	public String searchAll(Model model){
-		List<Document> docs = documentDAO.getAll();
-		model.addAttribute("docs", docs);
-		System.out.println("@@@@: " + docs.size());
+	public String searchAll(Model model){	
+		model.addAttribute("docs", documentDAO.getAll());
 		return "search/all";
 	}
 }
