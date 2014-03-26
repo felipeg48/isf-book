7f87505b5497e9e3722a5bc100e207b43b2b0f7c
felipeg48 <fgutierrezcru@vmware.com>
2014-03-25 22:35:23 -0500

    updated for CH16

--- ch16/src/main/java/com/apress/isf/spring/amqp/RabbitMQProducer.java
+++ ch16/src/main/java/com/apress/isf/spring/amqp/RabbitMQProducer.java
@@ -3,14 +3,11 @@
  */
 package com.apress.isf.spring.amqp;
 
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.apress.isf.java.model.Document;
-import com.apress.isf.java.utils.XmlUtils;
 
 /**
  * @author Felipe Gutierrez
@@ -18,15 +15,12 @@ import com.apress.isf.java.utils.XmlUtils;
  */
 @Component("rabbitmqProducer")
 public class RabbitMQProducer {
-
+	private static final String EXCHANGE = "mydocuments";
+	
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
 	
 	public void send(Document document){
-		MessageProperties messageProperties = new MessageProperties();
-		messageProperties.setContentType("text/xml");
-		byte[] body = XmlUtils.toXML(document).getBytes();
-		Message message = new Message(body, messageProperties);
-		rabbitTemplate.send(message);
+		rabbitTemplate.convertAndSend(EXCHANGE,document.getType().getExtension(),document);
 	}
 }
