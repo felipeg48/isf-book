ff474e0652f8d6fbbac896876c89bdefbaca2679
Felipe Gutierrez <felipeg@gmail.com>
2014-05-17 17:52:26 -0600

    added more to the CH17

--- ch17/src/main/java/com/apress/isf/spring/service/DocumetServiceFacade.java
+++ ch17/src/main/java/com/apress/isf/spring/service/DocumetServiceFacade.java
@@ -12,6 +12,7 @@ import com.apress.isf.java.model.Document;
 import com.apress.isf.java.model.Type;
 import com.apress.isf.java.service.DocumentService;
 import com.apress.isf.spring.data.DocumentDAO;
+import com.apress.isf.spring.social.DocumentTweet;
 
 /**
  * @author Felipe Gutierrez
@@ -23,12 +24,24 @@ public class DocumetServiceFacade implements DocumentService {
 	@Autowired
 	DocumentDAO documentDAO;
 	
+	@Autowired
+	DocumentTweet documentTweet;
+	
 	public List<Document> getAllDocuments(){
 		return documentDAO.getAll();
 	}
 	
 	public Document saveDocument(String id, Document document) {
-		return documentDAO.save(id, document);
+		Document documentResult = documentDAO.save(id, document);
+		
+		if("WEB".equals(documentResult.getType().getName())){
+			StringBuilder tweetText = new StringBuilder("My Documents App - A Book update: ");
+		 	tweetText.append(documentResult.getName());
+		 	tweetText.append(". Check it out: ");
+			tweetText.append(documentResult.getLocation());
+			documentTweet.tweet(tweetText.toString());
+		}
+		return documentResult;
 	}
 
 	public Document removeDocumentById(String id) {
